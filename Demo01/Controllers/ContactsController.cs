59611fce57a81bb139ac70ce300682f29a0cc134
MassimaMao <mastem@msn.com>
2024-01-29 11:40:34 +0100

    Ajoutez des fichiers projet.

---
+++ Demo01/Controllers/ContactsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Demo01.Controllers
+{
+    public class ContactsController : Controller
+    {
+        // /Contacts/       => possible grâce au app.MapControllerRoute("default",...) de program.cs
+        // /Contacts/Index
+        public string Index()
+        {
+            return "Je suis la page pour afficher les contacts";
+        }
+
+        // /Contacts/Details/5
+        public string Details(int id)
+        {
+            return $"Je suis la page pour afficher le contact n°{id} en détail...";
+        }
+
+        public string Add()
+        {
+            return "Je suis la page pour ajouter un contact";
+        }
+    }
+}
